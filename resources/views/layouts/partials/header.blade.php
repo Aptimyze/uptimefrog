64109b4d5044dc5bd4ab8be2f8e35f2d45ad09a3
Mario Pajunovic <contact@mariopajunovic.com>
2019-02-01 10:20:35 +0100

    Optmizing speed , delete CDN fonts.

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -8,11 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Uptime Frog') }} - @yield('title')</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
