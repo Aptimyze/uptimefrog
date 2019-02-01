f303b4c534cba07065830588a1040b91429e6ff8
Mario Pajunovic <contact@mariopajunovic.com>
2019-02-01 11:46:17 +0100

    Monitor index html table integration.

--- resources/views/service/monitors/index.blade.php
+++ resources/views/service/monitors/index.blade.php
@@ -7,15 +7,73 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">Monitors</div>
-
-            <div class="card-body">
-                @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                @endif
-
-                You are on monitor page.
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th scope="col" width="5%">#</th>
+                        <th scope="col" width="5%" class="text-center">Monitor</th>
+                        <th scope="col" class="text-left">Host / IP</th>
+                        <th scope="col" class="text-center">Port</th>
+                        <th scope="col" class="text-center">Type</th>
+                        <th scope="col" class="text-center">Latency</th>
+                        <th scope="col" width="12%" class="text-center">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td class="text-center">
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input" checked>
+                                <label class="custom-control-label"></label>
+                            </div>
+                        </td>
+                        <td>Mark</td>
+                        <td class="text-center">465</td>
+                        <td class="text-center">Website</td>
+                        <td class="text-center">0.0001</td>
+                        <td class="text-center">
+                            <button type="button" class="btn btn-sm btn-secondary">View</button>
+                            <button type="button" class="btn btn-sm btn-secondary">Edit</button>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td class="text-center">
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input">
+                                <label class="custom-control-label"></label>
+                            </div>
+                        </td>
+                        <td>Jacob</td>
+                        <td class="text-center">22</td>
+                        <td class="text-center">Server</td>
+                        <td class="text-center">0.0001</td>
+                        <td class="text-center">
+                            <button type="button" class="btn btn-sm btn-secondary">View</button>
+                            <button type="button" class="btn btn-sm btn-secondary">Edit</button>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td class="text-center">
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input">
+                                <label class="custom-control-label"></label>
+                            </div>
+                        </td>
+                        <td>Larry</td>
+                        <td class="text-center">21</td>
+                        <td class="text-center">FTP</td>
+                        <td class="text-center">0.0001</td>
+                        <td class="text-center">
+                            <button type="button" class="btn btn-sm btn-secondary">View</button>
+                            <button type="button" class="btn btn-sm btn-secondary">Edit</button>
+                        </td>
+                      </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
