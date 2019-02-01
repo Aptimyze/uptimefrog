1d62ada295dd38efbb13dad7f8e82af85051148d
Mario Pajunovic <contact@mariopajunovic.com>
2019-02-01 10:21:28 +0100

    Create Monitor Controller

---
+++ app/Http/Controllers/Service/MonitorController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace UptimeFrog\Http\Controllers\Service;
+
+use Illuminate\Http\Request;
+use UptimeFrog\Http\Controllers\Controller;
+
+class MonitorController extends Controller
+{
+    //
+}
